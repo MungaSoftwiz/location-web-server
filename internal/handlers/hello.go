11d051b2d03d13da824c4c159e2047449c0f4fe2
MungaSoftwiz <mwachilumobm@gmail.com>
2024-07-02 01:39:10 +0300

    refactor: add error handling

--- internal/handlers/hello.go
+++ internal/handlers/hello.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/MungaSoftwiz/location-web-server/internal/utils"
 	"github.com/MungaSoftwiz/location-web-server/internal/services"
+	"github.com/MungaSoftwiz/location-web-server/internal/utils"
 )
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,8 +16,19 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	clientIP := utils.GetClientIP(r)
-	location, _ := services.GetLocation(clientIP)
-	weatherData, _ := services.GetWeatherData(location.Latitude, location.Longitude)
+	location, err := services.GetLocation(clientIP)
+	if err != nil {
+		log.Printf("Error getting location: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	weatherData, err := services.GetWeatherData(location.Latitude, location.Longitude)
+	if err != nil {
+		log.Printf("Error getting weather data: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 	response := map[string]interface{}{
 		"client_ip": clientIP,
