3d89005abd8da6eed73fb07ad5819689038dd346
MungaSoftwiz <mwachilumobm@gmail.com>
2024-07-02 09:51:41 +0300

    refactor: get port from railway env or :4000

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/MungaSoftwiz/location-web-server/internal/handlers"
 )
@@ -12,7 +13,13 @@ func main() {
 	mux.HandleFunc("/api/hello", handlers.HelloHandler)
 
 	log.Println("Starting server on :4000")
-	err := http.ListenAndServe("0.0.0.0:4000", mux)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "4000"
+	}
+
+	err := http.ListenAndServe("0.0.0.0:"+port, mux)
 	if err != nil {
 		log.Fatal(err)
 	}
